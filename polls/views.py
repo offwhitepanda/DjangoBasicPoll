24b96d1b80dd5aca7428b85872c14b9558ba9d6b
offwhitepanda <hurleyint_09@yahoo.com>
2023-05-21 14:24:40 +0900

    Added simple homepage

--- polls/views.py
+++ polls/views.py
@@ -12,7 +12,14 @@ def index(request):
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        try:
+            raise Http404("Question does not exist")
+        except:
+            return render(request, "polls/polls404.html", {"questionID:": question_id})
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
